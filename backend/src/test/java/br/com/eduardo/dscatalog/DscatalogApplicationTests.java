e5cb56bb887e63d55c6b0ae3030582abb50591c2
Eduardo Alves dos Santos <santosedu@live.com>
2023-09-10 23:18:52 -0300

    Backend project created

---
+++ backend/src/test/java/br/com/eduardo/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduardo.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
