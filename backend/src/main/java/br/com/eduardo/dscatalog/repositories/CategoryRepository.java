82db7a26e0795946d5fa691941bda9a88c21227d
Eduardo Alves dos Santos <santosedu@live.com>
2023-09-27 11:21:03 -0300

    Including some new dependencies on pom.xml

--- backend/src/main/java/br/com/eduardo/dscatalog/repositories/CategoryRepository.java
+++ backend/src/main/java/br/com/eduardo/dscatalog/repositories/CategoryRepository.java
@@ -1,9 +1,20 @@
 package br.com.eduardo.dscatalog.repositories;
 
 import br.com.eduardo.dscatalog.entities.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    @Query("FROM Category c " +
+            "WHERE LOWER(c.name) like %:searchTerm% ")
+    Page<Category> search(
+            @Param("searchTerm") String searchTerm,
+            Pageable pageable);
+
+//    Page<Category> listSorted(Long id, Pageable pageable);
 }
