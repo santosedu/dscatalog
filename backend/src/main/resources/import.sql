998757d6b307f49640a2246288ab08d0cd0c725c
Eduardo Alves dos Santos <santosedu@live.com>
2023-09-29 18:06:42 -0300

    Seeding database file

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into tb_category(name) values('Books');
-insert into tb_category(name) values('Eletronics');
-insert into tb_category(name) values('Computers');
+insert into tb_category(name, created_At) values('Books', NOW());
+insert into tb_category(name, created_At) values('Eletronics', NOW());
+insert into tb_category(name, created_At) values('Computers', NOW());
